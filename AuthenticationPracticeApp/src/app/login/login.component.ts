86e4c834092ea23e1dd427df3a3561a5dc3e0617
Priya Kumari <priyakumari0112@gmail.com>
2021-04-04 13:43:52 +0530

    Implemented login

--- AuthenticationPracticeApp/src/app/login/login.component.ts
+++ AuthenticationPracticeApp/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  fs = firebase.default.firestore();
+
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  async submitCredentials() {
+    let self = this;
+    let userProfile = null;
+    await this.fs.collection('profiles').get().then((profiles)=> {
+
+      profiles.forEach(function(p) {
+        console.log(p.data().email);
+        if(p.data().email == self.loginForm.get('email').value) {
+          userProfile = p.data();
+        }
+      });
+    });
+    console.log(userProfile);
+    if((userProfile && userProfile.password as string) == (this.loginForm.get('password').value as string)){
+      alert('loginSuccessful')
+    }
+    else {
+      alert('loginfailed')
+    }
+  }
 }
